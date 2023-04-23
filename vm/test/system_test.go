afabd9a8a3987bcdfaf14c679bd526e32c3ee755
certainty <david.krentzlin@gmail.com>
2023-04-23 13:35:56 +0200

    Add tests stubs

---
+++ vm/test/system_test.go
@@ -0,0 +1,10 @@
+package vm_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSystemExample(t *testing.T) {
+	assert.Equal(t, true, true, "System test for ExampleFunction should return the expected result")
+}
