75d4473944b0d9f4106ee53da7755855efbac2c3
certainty <david.krentzlin@gmail.com>
2023-04-25 08:57:36 +0200

    WIP

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -1,1 +1,31 @@
 package repl
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/knz/bubbline/editline"
+)
+
+type Repl struct {
+	editline *editline.Model
+}
+
+func NewRepl(width, height int) *Repl {
+	return &Repl{
+		editline: editline.New(width, height),
+	}
+}
+
+func (r *Repl) Init() tea.Cmd {
+	return r.editline.Init()
+}
+
+func (r *Repl) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m, cmd := r.editline.Update(msg)
+	r.editline = m.(*editline.Model)
+	return r, cmd
+}
+
+func (r *Repl) View() string {
+	return r.editline.View()
+}
